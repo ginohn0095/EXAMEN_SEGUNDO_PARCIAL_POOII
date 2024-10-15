762f78329100aacc857c60fe6c3d65d4a5da2b4a
GINO HIGUERA NAVARRA <112723@alumnouninter.mx>
2024-10-15 16:41:26 -0600

    Agregar archivos de proyecto.

---
+++ EXAMEN_SEGUNDO_PARCIAL_POOII/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace EXAMEN_SEGUNDO_PARCIAL_POOII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNAGRAGAR = new System.Windows.Forms.Button();
+            this.BTNELIMINAR = new System.Windows.Forms.Button();
+            this.BTNACTUALIZAR = new System.Windows.Forms.Button();
+            this.BTNMOSTRAR = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.V = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTNAGRAGAR
+            // 
+            this.BTNAGRAGAR.Location = new System.Drawing.Point(1288, 79);
+            this.BTNAGRAGAR.Name = "BTNAGRAGAR";
+            this.BTNAGRAGAR.Size = new System.Drawing.Size(184, 73);
+            this.BTNAGRAGAR.TabIndex = 0;
+            this.BTNAGRAGAR.Text = "AGREGAR";
+            this.BTNAGRAGAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNELIMINAR
+            // 
+            this.BTNELIMINAR.Location = new System.Drawing.Point(1288, 378);
+            this.BTNELIMINAR.Name = "BTNELIMINAR";
+            this.BTNELIMINAR.Size = new System.Drawing.Size(184, 67);
+            this.BTNELIMINAR.TabIndex = 1;
+            this.BTNELIMINAR.Text = "ELIMINAR";
+            this.BTNELIMINAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNACTUALIZAR
+            // 
+            this.BTNACTUALIZAR.Location = new System.Drawing.Point(1288, 287);
+            this.BTNACTUALIZAR.Name = "BTNACTUALIZAR";
+            this.BTNACTUALIZAR.Size = new System.Drawing.Size(184, 62);
+            this.BTNACTUALIZAR.TabIndex = 2;
+            this.BTNACTUALIZAR.Text = "ACTUALIZAR";
+            this.BTNACTUALIZAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNMOSTRAR
+            // 
+            this.BTNMOSTRAR.Location = new System.Drawing.Point(1288, 186);
+            this.BTNMOSTRAR.Name = "BTNMOSTRAR";
+            this.BTNMOSTRAR.Size = new System.Drawing.Size(184, 62);
+            this.BTNMOSTRAR.TabIndex = 3;
+            this.BTNMOSTRAR.Text = "MOSTRAR";
+            this.BTNMOSTRAR.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(96, 467);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1376, 433);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(863, 264);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(863, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(258, 29);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(213, 264);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(258, 29);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // V
+            // 
+            this.V.AutoSize = true;
+            this.V.Location = new System.Drawing.Point(91, 268);
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(64, 25);
+            this.V.TabIndex = 10;
+            this.V.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(746, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(746, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "ID";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(199, 356);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(258, 29);
+            this.textBox5.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1577, 936);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.V);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BTNMOSTRAR);
+            this.Controls.Add(this.BTNACTUALIZAR);
+            this.Controls.Add(this.BTNELIMINAR);
+            this.Controls.Add(this.BTNAGRAGAR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNAGRAGAR;
+        private System.Windows.Forms.Button BTNELIMINAR;
+        private System.Windows.Forms.Button BTNACTUALIZAR;
+        private System.Windows.Forms.Button BTNMOSTRAR;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label V;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
